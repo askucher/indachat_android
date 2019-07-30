3296226afd7741deca2caae4a4bfb893baebba70
Kostya Pogromskiy <kpogromskiy@gmail.com>
2019-07-30 15:33:45 +0300

    Initial

---
+++ TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
@@ -0,0 +1,178 @@
+/*
+ * This is the source code of Telegram for Android v. 3.x.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2017.
+ */
+
+package org.telegram.messenger;
+
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.Application;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ApplicationInfo;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.telegram.tgnet.ConnectionsManager;
+import org.telegram.tgnet.TLRPC;
+import org.telegram.ui.Components.ForegroundDetector;
+
+import java.io.File;
+
+public class ApplicationLoader extends Application {
+
+    private static PendingIntent pendingIntent;
+
+    @SuppressLint("StaticFieldLeak")
+    public static volatile Context applicationContext;
+    public static volatile Handler applicationHandler;
+    private static volatile boolean applicationInited = false;
+
+    public static volatile boolean isScreenOn = false;
+    public static volatile boolean mainInterfacePaused = true;
+    public static volatile boolean externalInterfacePaused = true;
+    public static volatile boolean mainInterfacePausedStageQueue = true;
+    public static volatile long mainInterfacePausedStageQueueTime;
+
+    public static File getFilesDirFixed() {
+        for (int a = 0; a < 10; a++) {
+            File path = ApplicationLoader.applicationContext.getFilesDir();
+            if (path != null) {
+                return path;
+            }
+        }
+        try {
+            ApplicationInfo info = applicationContext.getApplicationInfo();
+            File path = new File(info.dataDir, "files");
+            path.mkdirs();
+            return path;
+        } catch (Exception e) {
+            FileLog.e(e);
+        }
+        return new File("/data/data/org.telegram.messenger/files");
+    }
+
+    public static void postInitApplication() {
+        if (applicationInited) {
+            return;
+        }
+
+        applicationInited = true;
+
+        try {
+            LocaleController.getInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            final IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
+            final BroadcastReceiver mReceiver = new ScreenReceiver();
+            applicationContext.registerReceiver(mReceiver, filter);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            PowerManager pm = (PowerManager)ApplicationLoader.applicationContext.getSystemService(Context.POWER_SERVICE);
+            isScreenOn = pm.isScreenOn();
+            if (BuildVars.LOGS_ENABLED) {
+                FileLog.d("screen state = " + isScreenOn);
+            }
+        } catch (Exception e) {
+            FileLog.e(e);
+        }
+
+        SharedConfig.loadConfig();
+        for (int a = 0; a < UserConfig.MAX_ACCOUNT_COUNT; a++) {
+            UserConfig.getInstance(a).loadConfig();
+            MessagesController.getInstance(a);
+            ConnectionsManager.getInstance(a);
+            TLRPC.User user = UserConfig.getInstance(a).getCurrentUser();
+            if (user != null) {
+                MessagesController.getInstance(a).putUser(user, true);
+                MessagesController.getInstance(a).getBlockedUsers(true);
+                SendMessagesHelper.getInstance(a).checkUnsentMessages();
+            }
+        }
+
+        ApplicationLoader app = (ApplicationLoader)ApplicationLoader.applicationContext;
+        if (BuildVars.LOGS_ENABLED) {
+            FileLog.d("app initied");
+        }
+
+        MediaController.getInstance();
+        for (int a = 0; a < UserConfig.MAX_ACCOUNT_COUNT; a++) {
+            ContactsController.getInstance(a).checkAppAccount();
+            DownloadController.getInstance(a);
+        }
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        applicationContext = getApplicationContext();
+        NativeLoader.initNativeLibs(ApplicationLoader.applicationContext);
+        ConnectionsManager.native_setJava(false);
+        new ForegroundDetector(this);
+
+        applicationHandler = new Handler(applicationContext.getMainLooper());
+
+        AndroidUtilities.runOnUIThread(ApplicationLoader::startPushService);
+    }
+
+    public static void startPushService() {
+        Log.d("TFOSS", "Trying to start push service every 10 minutes");
+        // Telegram-FOSS: unconditionally enable push service
+        AlarmManager am = (AlarmManager) applicationContext.getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(applicationContext, ApplicationLoader.class);
+        pendingIntent = PendingIntent.getBroadcast(applicationContext, 0, i, 0);
+
+        am.cancel(pendingIntent);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 60000, pendingIntent);
+        try {
+            Log.d("TFOSS", "Starting push service...");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                applicationContext.startForegroundService(new Intent(applicationContext, NotificationsService.class));
+            } else {
+                applicationContext.startService(new Intent(applicationContext, NotificationsService.class));
+            }
+        } catch (Throwable e) {
+            Log.d("TFOSS", "Failed to start push service");
+        }
+    }
+
+    public static void stopPushService() {
+        applicationContext.stopService(new Intent(applicationContext, NotificationsService.class));
+
+        PendingIntent pintent = PendingIntent.getService(applicationContext, 0, new Intent(applicationContext, NotificationsService.class), 0);
+        AlarmManager alarm = (AlarmManager)applicationContext.getSystemService(Context.ALARM_SERVICE);
+        alarm.cancel(pintent);
+        alarm.cancel(pendingIntent);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        try {
+            LocaleController.getInstance().onDeviceConfigurationChange(newConfig);
+            AndroidUtilities.checkDisplaySize(applicationContext, newConfig);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
